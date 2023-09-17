9fb7b2bf261e2db10c55c5e0981894c13795430e
DashnyamB <lord_dashka277@yahoo.com>
2023-09-17 16:08:20 +0800

    fix: add cart icon

--- src/generated/index.ts
+++ src/generated/index.ts
@@ -1,3 +1,4 @@
 export { default as BurgerMenu } from './BurgerMenu';
 export { default as Checked } from './Checked';
 export { default as Home } from './Home';
+export { default as ShoppingCart } from './ShoppingCart';
